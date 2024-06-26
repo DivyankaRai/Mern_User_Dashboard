fa239974d4fb482dba9aee39d559ce6b58d4d93c
divyanka05 <divyanka@cashflowy.io>
2024-06-26 15:32:41 +0530

    made some changes in twilio.js file

--- server/twilio.js
+++ server/twilio.js
@@ -1,7 +1,7 @@
 const twilio = require('twilio');
 
-const accountSid = 'your_account_sid';
-const authToken = 'your_auth_token'
+const accountSid = 'ACc52e8add86d970e48030a27e6b785a0c';
+const authToken = '7cba068948c520ab1a6d99ae5c70bd0e'
 
 console.log(`Account SID: ${accountSid}`);
 console.log(`Auth Token: ${authToken}`);
